ec3adae9a949fb5c8dcb1f63f3e831b64807ef4a
SistemasCAE <sistemas_cae@cae3076.com>
2019-11-07 16:39:43 -0600

    Actualizacion config

--- js/API/barcodeScanner.js
+++ js/API/barcodeScanner.js
@@ -1,31 +1,31 @@
 var bcs = {
 	abrirCamara: function(){
-		window.plugins.toast.show("aquiiiiiiiiiiii", 'long', 'center');
+		//window.plugins.toast.show("aquiiiiiiiiiiii", 'long', 'center');
 			cordova.plugins.barcodeScanner.scan(
-		  function (result) {
-			  alert("We got a barcode\n" +
-					"Result: " + result.text + "\n" +
-					"Format: " + result.format + "\n" +
-					"Cancelled: " + result.cancelled);
-		  },
-		  function (error) {
-			  alert("Scanning failed: " + error);
-		  },
-		  {
-			  preferFrontCamera : true, // iOS and Android
-			  showFlipCameraButton : true, // iOS and Android
-			  showTorchButton : true, // iOS and Android
-			  torchOn: true, // Android, launch with the torch switched on (if available)
-			  saveHistory: true, // Android, save scan history (default false)
-			  prompt : "Place a barcode inside the scan area", // Android
-			  resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-			  formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-			  orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
-			  disableAnimations : true, // iOS
-			  disableSuccessBeep: false // iOS and Android
-		  }
-	   );
-		window.plugins.toast.show("aquiiiiiiiiiiii22222222222222222222", 'long', 'center');
+			  function (result) {
+				  alert("We got a barcode\n" +
+						"Result: " + result.text + "\n" +
+						"Format: " + result.format + "\n" +
+						"Cancelled: " + result.cancelled);
+			  },
+			  function (error) {
+				  alert("Scanning failed: " + error);
+			  },
+			  {
+				  preferFrontCamera : true, // iOS and Android
+				  showFlipCameraButton : true, // iOS and Android
+				  showTorchButton : true, // iOS and Android
+				  torchOn: true, // Android, launch with the torch switched on (if available)
+				  saveHistory: true, // Android, save scan history (default false)
+				  prompt : "Place a barcode inside the scan area", // Android
+				  resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+				  formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+				  orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
+				  disableAnimations : true, // iOS
+				  disableSuccessBeep: false // iOS and Android
+			  }
+		   );
+			window.plugins.toast.show("aquiiiiiiiiiiii22222222222222222222", 'long', 'center');
 		/*cordova.plugins.barcodeScanner.scan(
 			function (result) {
 				if(result.text != ""){
