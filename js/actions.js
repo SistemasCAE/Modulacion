820a985cd87323b34b4ed62c04ccfde9bb8d723f
SistemasCAE <sistemas_cae@cae3076.com>
2019-11-08 17:24:24 -0600

    pruebas barcodescaner

--- js/actions.js
+++ js/actions.js
@@ -10,8 +10,8 @@ var fn = {
 		 */
 		//bcs.abrirCamara();
 		fn.compruebaSesion();
-		//$("#botonEscanea").tap(bcs.abrirCamara););
-		$("#botonEscanea").tap(fn.abreCamara);
+		//$("#botonEscanea").tap(bcs.abrirCamara);
+		$("#botonEscanea").tap(fn.abrirCamara);
 		$("#botonIniciarSesion").tap(fn.iniciarSesion);
 		$("#botonCerrarSesion").tap(fn.cerrarSesion);
 		$("#botonPendientes").tap(almacena.cargarDatosPendientes);
@@ -25,36 +25,15 @@ var fn = {
 		navigator.app.exitApp();
 		return false;
 	},
-	
-	abreCamara : function(){
-	
-	window.plugins.toast.show("abreCamaraaaa", 'long', 'center');
-		  /*cordova.plugins.barcodeScanner.scan(
-		  function (result) {
-			  alert("We got a barcode\n" +
-					"Result: " + result.text + "\n" +
-					"Format: " + result.format + "\n" +
-					"Cancelled: " + result.cancelled);
-		  },
-		  function (error) {
-			  alert("Scanning failed: " + error);
-		  },
-		  {
-			  "preferFrontCamera" : true, // iOS and Android
-			  "showFlipCameraButton" : true, // iOS and Android
-			  "prompt" : "Place a barcode inside the scan area", // supported on Android only
-			  "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
-			  "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
-		  }
-	   );*/
-	}.
-	
-	
 	cerrarSesion: function(){
 		window.localStorage.removeItem("nombreUsuario");
 		$("#usuarioSesion").val("");
 		$("#passwordSesion").val(""); 
 		window.location.href = "#inicioSesion";
+	},
+	
+	abrirCamara: function(){
+		window.plugins.toast.show("aquiiiiiiiiiiii abre camaraaaa", 'long', 'center');
 	},
 	iniciarSesion: function(){
 		var usuario = $("#usuarioSesion").val();
