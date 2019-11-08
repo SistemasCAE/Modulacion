fad4bea648cfebb0f219e2d62870968ee58faf15
SistemasCAE <sistemas_cae@cae3076.com>
2019-11-08 17:27:31 -0600

    pruebas barcodescaner

--- js/actions.js
+++ js/actions.js
@@ -34,6 +34,24 @@ var fn = {
 	
 	abrirCamara: function(){
 		window.plugins.toast.show("aquiiiiiiiiiiii abre camaraaaa", 'long', 'center');
+		cordova.plugins.barcodeScanner.scan(
+			function (result) {
+			  alert("We got a barcode\n" +
+					"Result: " + result.text + "\n" +
+					"Format: " + result.format + "\n" +
+					"Cancelled: " + result.cancelled);
+			},
+			function (error) {
+			  alert("Scanning failed: " + error);
+			},
+			{
+			  "preferFrontCamera" : true, // iOS and Android
+			  "showFlipCameraButton" : true, // iOS and Android
+			  "prompt" : "Place a barcode inside the scan area", // supported on Android only
+			  "formats" : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+			  "orientation" : "landscape" // Android only (portrait|landscape), default unset so it rotates with the device
+			}
+		);
 	},
 	iniciarSesion: function(){
 		var usuario = $("#usuarioSesion").val();
